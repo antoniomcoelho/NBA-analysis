fad8e0c18f3592776ecaf0e2a4a55f78d6090590
amcoelho <antonio.m.coelho@inesctec.pt>
2023-01-31 17:45:40 +0000

    add terraform main.tf and variables.tf

---
+++ Terraform/main.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("C:\\Users\\amfcc\\MyDocuments\\Data Engineering\\1 - MyProject - NBA keys\\nba-analysis-375915-43108b812f5b.json") 
+
+  project = "nba-analysis-375915"
+  region  = "europe-west6"
+}
+
+
+# Data Lake Bucket
+# Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket
+resource "google_storage_bucket" "data-lake-bucket" {
+  name          = "${local.data_lake_bucket}_${var.project}" # Concatenating DL bucket & Project name for unique naming
+  location      = var.region
+
+  # Optional, but recommended settings:
+  storage_class = var.storage_class
+  #uniform_bucket_level_access = true
+
+  versioning {
+    enabled     = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30  // days
+    }
+  }
+
+  force_destroy = true
+}
+
+# DWH
+# Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/bigquery_dataset
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id = var.BQ_DATASET
+  project    = var.project
+  location   = var.region
+  }
